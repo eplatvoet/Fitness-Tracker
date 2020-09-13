e9357627d5ec56eb492e42ec78de0ff527d5ffdf
Erin Platvoet <erin.didomenico520@gmail.com>
2020-09-12 21:39:13 -0400

    several routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,71 +5,42 @@ const { red } = require('color-name');
 // Use 'db.Workout' to refernce the model and use the methods provided with the model to execute database operatioms
 
 router.post('/api/workouts', (req, res) => {
-  // Replace <METHOD> with method to create document in collection with data in req body
-  // Pass in data passed in from browser as input argument to the method
   db.Workout.create(body)
-
-    // Fill in .then() with call back function that takes result from db as input argument and send it back to browser
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
-
-    // Fill in .catch() with call back function that takes error as input argument and send it back to browser
     .catch(err => {
         res.status(400).json(err);
     });
 });
 
 router.put('/api/workouts/:id', (req, res) => {
-  // Replace <METHOD> with method to Find the document with id passed in and update the document with data in req body
-  // Look into mongoose doc for method to perform both find document by id and update the document
-  // Look into a way to push data passed in to the exercises array in model
-  // Fill in the input argument(s) to the method
-
-  db.Workout.find(
-    
-    // "runValidators" will ensure new exercises meet our schema requirements
-    // Don't forget comma proceeds the following option
-    { new: true, runValidators: true }
-  )
-
-    // Fill in .then() with call back function that takes result from db as input argument and send it back to browser
+  db.Workout.find({ new: true, runValidators: true })
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
-
-    // Fill in .catch() with call back function that takes error as input argument and send it back to browser
     .catch(err => {
         res.status(400).json(err);
     });
 });
 
 router.get('/api/workouts', (req, res) => {
-  // Replace <METHOD> with method to Find all workouts from collection
   db.Workout.find({})
-
-    // Fill in .then() with call back function that takes result from db as input argument and send it back to browser
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
-
-    // Fill in .catch() with call back function that takes error as input argument and send it back to browser
     .catch(err => {
         res.status(400).json(err);
     });
 });
 
 router.get('/api/workouts/range', (req, res) => {
-  // Replace <METHOD> with method to Find all workouts then limit with range
-  // Look into mongoose doc for how to limit with range
-  // Fill in the input argument(s) to the method
-
-  db.Workout.find({}).limit()
-
-    // Fill in .then() with call back function that takes result from db as input argument and send it back to browser
-    .then()
-
-    // Fill in .catch() with call back function that takes error as input argument and send it back to browser
+  db.Workout.find({})
+    .sort({ date: -1 })
+    .limit(7)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
     .catch(err => {
         res.status(400).json(err);
     });
@@ -81,7 +52,7 @@ router.delete('/api/workouts', ({ body }, res) => {
   // Review the front end javascript code to understand how document id is passed to back end
   // Fill in the input argument(s) to the method
 
-  db.Workout.<METHOD>()
+  db.Workout.findOneAndDelete({ _id: id })
 
     // Fill in .then() with call back function that takes no input argument and send boolean 'true' back to browser
     .then()
